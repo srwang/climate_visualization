d2eacf6078fba8c2aaf9168d2901677e7925e264
Sharon Wag <sharon.ran.wang@gmail.com>
2016-05-21 18:38:31 -0400

    expanded on map base

--- js/main.js
+++ js/main.js
@@ -1,46 +1,57 @@
 console.log('linked');
 
-//create basic map (3-D globe or flat visuals)
-	//flat map of globe (split)
-	//flora/fauna to coincide with some major areas - i.e. coral bleaching australia coast
-	//have a 
-//create a range slider
-
-//map build tool
-//kml data (?)
-
-//iterate through the countries (?)
-
-//read about natural earth
-//grab all the countries' data (?)
-//split globe- create a circle svg or background on which the svgs sit
-
-//make an api call and store it somewhere- either in session or in database 
-//
-
 //create SVG
-var width = 960,
-    height = 1160;
+var width = $(window).width(),
+    height = 800;
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = d3.select('#map').append('svg')
+    .attr('width', width)
+    .attr('height', height);
 
 var startYear = 2020,
 	endYear = 2039,
 	countryCode = 'grl';
 
-//grab json and render map
+//render map
+var projection = d3.geo.stereographic()
+    .scale(200)
+    .translate([width / 2, height / 2])
+    .rotate([-20, 0])
+    .clipAngle(130)
+    .clipExtent([[0, 0], [width, height]])
+    .precision(.1);
+	//adjust
+
+var path = d3.geo.path()
+    .projection(projection);
+
 d3.json('map_data/world.json', function(error, world) {
 	if (error) return console.error(error);
 	console.log(world);
 
-	svg.append("path")
-		.datum(topojson.feature(world, world.objects.subunits))
-		.attr("d", d3.geo.path().projection(d3.geo.mercator()));
+	var subunits = topojson.feature(world, world.objects.subunits);
+
+	svg.append('path')
+    .datum(subunits)
+    .attr('d', path);
+
+	svg.selectAll('.subunit')
+	    .data(topojson.feature(world, world.objects.subunits).features)
+	  .enter().append('path')
+	    .attr('class', 
+	    	function (d) { 
+	    		return 'subunit ' + d.id; 
+	    	})
+	    .attr('d', path);
+
+	svg.selectAll(".subunit-label")
+    .data(topojson.feature(uk, uk.objects.subunits).features)
+  .enter().append("text")
+    .attr("class", function(d) { return "subunit-label " + d.id; })
+    .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .text(function(d) { return d.properties.name; });
 });
-
-
 
 //api call
 d3.jsonp('http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/' + startYear + '/' + endYear + '/' + countryCode + '?callback=handler');
