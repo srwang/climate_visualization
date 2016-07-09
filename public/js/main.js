db50a031471dc2003dcfbdc3900d3b397e45d9e6
Sharon Wag <sharon.ran.wang@gmail.com>
2016-07-09 16:58:35 -0400

    rotation working

--- public/js/main.js
+++ public/js/main.js
@@ -11,13 +11,10 @@ var mousePosition0;
 var svg = d3.select('#map').append('svg')
     .attr('width', width)
     .attr('height', height)
-    // .on('mousedown', function(){
-	   //  	mousePosition0 = [d3.event.pageX, d3.event.pageY];
-	   //  });
 
 //set map properties
-var projection = d3.geoOrthographic()
-    .scale(300)
+var projection = d3.geoStereographic()
+    .scale(250)
     .center([0, 0])
     .translate([width / 2, height / 2])
     .rotate([0,0,0])
@@ -35,11 +32,6 @@ d3.json('map_data/new_world.json', function (error, world) {
 	if (error) return console.log(error);
 
 	var subunits = topojson.feature(world, world.objects.subunits);
-
-    //create countries
-	svg.append('path')
-    .datum(subunits)
-    .attr('d', path);
 
 	feature = svg.selectAll('.subunit')
 	    .data(topojson.feature(world, world.objects.subunits).features)
@@ -59,7 +51,7 @@ d3.json('map_data/new_world.json', function (error, world) {
 	    .attr('d', path);
 
 	//create labels
-	svg.selectAll('.subunit-label')
+	label = svg.selectAll('.subunit-label')
 		.data(topojson.feature(world, world.objects.subunits).features)
 	.enter().append('text')
 		.attr('class', 'subunit-label')
@@ -74,7 +66,9 @@ d3.json('map_data/new_world.json', function (error, world) {
 		.attr('transform', function(d) { 
 			var center = path.centroid(d);
 			//adjust for left offset
-			return 'translate(' + [center[0] - 20, center[1]] + ')';
+			if (!isNaN(center[0])){
+				return 'translate(' + [center[0] - 20, center[1]] + ')';
+			}
 		})
 		.attr('dy', '.1em')
 		.style('fill', 'black')
@@ -82,127 +76,90 @@ d3.json('map_data/new_world.json', function (error, world) {
 		.text(function(d) { return d.properties.name; });
 
 	//display labels on hover
-	// svg.selectAll('.subunit')
-	// 	.on('mouseenter', function(){ 
-	// 		svg.select('.subunit-label#' + this.id)
-	// 			.transition()
-	// 			.delay(200)
-	// 			.style('display', 'block');
-	// 	})
-	// 	.on('mouseleave', function(){
-	// 		svg.select('.subunit-label#' + this.id)
-	// 			.transition()
-	// 			.delay(200)
-	// 			.style('display', 'none');
-	// 	});
+	svg.selectAll('.subunit')
+		.on('mouseenter', function(){ 
+			svg.select('.subunit-label#' + this.id)
+				.transition()
+				.delay(200)
+				.style('display', 'block');
+		})
+		.on('mouseleave', function(){
+			svg.select('.subunit-label#' + this.id)
+				.transition()
+				.delay(200)
+				.style('display', 'none');
+		});
 
 });	
 
+//figure out render speed
+//add background SVG circle 
 
+var mousePosition0;
 
-//fix rotation problem - 3 hours
-//put in modals with climate info - 1.5 hr
+svg.on('mousedown', function(){
+	mousePosition0 = [d3.event.pageX, d3.event.pageY];
+});
 
-//read projection info online
-//study 
+svg.on('mousemove', function(){
+	if (mousePosition0) {
+		console.log(d3.event.pagX, d3.event.pageY);
 
+		var currentCenter = projection.rotate(),
+			mousePosition1 = [d3.event.pageX, d3.event.pageY],
+			newCenter = [currentCenter[0] + (mousePosition0[0]-mousePosition1[0]) / 8, currentCenter[1] + (mousePosition1[1]-mousePosition0[1]) / 8];
 
-//click and drag
-	
-//begin animation on mousedown
-//change position on mousemove
-//stop animation on mouseup
+		d3.event.preventDefault();
 
-//don't return totally new position, but rather, different angle(??)
-svg.on('mouseup', function(){
-
-
-		// if (mousePosition0) {
-		// 	var currentCenter = projection.rotate(),
-		// 		mousePosition1 = [d3.event.pageX, d3.event.pageY],
-		// 		newCenter = [currentCenter[0] + (mousePosition0[0]-mousePosition1[0]) / 8, currentCenter[1] + (mousePosition1[1]-mousePosition0[1]) / 8];
-		// }
-
-		console.log(feature.attr('d'));
-
-	    projection.rotate([-d3.event.pageX, -d3.event.pageY, 0]);
+	    projection.rotate([-newCenter[0], -newCenter[1], 0]);
 
 		feature.attr('d', d3.geoPath().projection(projection));
 
-		console.log(feature.attr('d'));
+		label.attr('transform', function(d) { 
+			var center = path.centroid(d);
+			if (!isNaN(center[0])){
+				return 'translate(' + [center[0] - 20, center[1]] + ')';
+			}
+		});
+	}
+});	
 
-		//calcColors();
-	});	
-
-
-
-// svg.selectAll('.subunit')
-// 	.transition()
-// 	.on('start', 'mouseup')
-// 	.tween('rotate', function(){
-// 		var center = [d3.event.pageX, d3.event.pageY],
-// 			r = d3.interpolate(projection.rotate(), [-center[0], -center[1]]);
-// 		return function(t) {
-// 			projection.rotate(r(t));
-// 		}
-// 	});
-
-//   (function transition() {
-//     d3.transition()
-//         .duration(1250)
-//         .each("start", function() {
-//           title.text(countries[i = (i + 1) % n].name);
-//         })
-//         .tween("rotate", function() {
-//           var p = d3.geo.centroid(countries[i]),
-//               r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
-//           return function(t) {
-//             projection.rotate(r(t));
-//             c.clearRect(0, 0, width, height);
-//             c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
-//             c.fillStyle = "#f00", c.beginPath(), path(countries[i]), c.fill();
-//             c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
-//             c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
-//           };
-//         })
-//       .transition()
-//         .each("end", transition);
-//   })();
-// }
-
+svg.on('mouseup', function(){
+	mousePosition0=null;
+});
 
 //COLORS
 //set map to colors corresponding to 2020 temps
-// changeMapColor([2020, 2039],
-// 	function(yearTwentyTemp, yearTwentyColor, countryCode){
-// 		setSvgFill(countryCode, yearTwentyColor);	
-// });
+changeMapColor([2020, 2039],
+	function(yearTwentyTemp, yearTwentyColor, countryCode){
+		setSvgFill(countryCode, yearTwentyColor);	
+});
 
-// //as temp changes over time, will increment or decrement reds to make change more visible
-// $('#year-selector').change(function(){
-// 	var range = findYearRange();
-// 	$('#selector-label').text(range[0] + ' -- ' + range[1]);
+//as temp changes over time, will increment or decrement reds to make change more visible
+$('#year-selector').change(function(){
+	var range = findYearRange();
+	$('#selector-label').text(range[0] + ' -- ' + range[1]);
 
-// 	if (range[0] === 2020) {
-// 		changeMapColor(range, function(yearTwentyTemp, yearTwentyColor, countryCode){
-// 				setSvgFill(countryCode, yearTwentyColor);	
-// 		});
-// 	} else {
-// 		changeMapColor(range, function(currentTemp, countryCode) {
-// 			//make a second call per country, to find 2020 temp and colors
-// 			makeApiCall(countryCode, [2020, 2039], function(yearTwentyTemp, yearTwentyColor, countryCode){
+	if (range[0] === 2020) {
+		changeMapColor(range, function(yearTwentyTemp, yearTwentyColor, countryCode){
+				setSvgFill(countryCode, yearTwentyColor);	
+		});
+	} else {
+		changeMapColor(range, function(currentTemp, countryCode) {
+			//make a second call per country, to find 2020 temp and colors
+			makeApiCall(countryCode, [2020, 2039], function(yearTwentyTemp, yearTwentyColor, countryCode){
 
-// 				var tempDiff = currentTemp - yearTwentyTemp,
-// 					diffMult = Math.floor(tempDiff/0.5),
-// 					currentColor = [yearTwentyColor[0]+(15*diffMult), yearTwentyColor[1], yearTwentyColor[2]-(10*diffMult)];
+				var tempDiff = currentTemp - yearTwentyTemp,
+					diffMult = Math.floor(tempDiff/0.5),
+					currentColor = [yearTwentyColor[0]+(15*diffMult), yearTwentyColor[1], yearTwentyColor[2]-(10*diffMult)];
 
-// 				if(!isNaN(yearTwentyTemp)) {
-// 					setSvgFill(countryCode, currentColor);
-// 				}
-// 			});
-// 		});
-// 	}
-// });
+				if(!isNaN(yearTwentyTemp)) {
+					setSvgFill(countryCode, currentColor);
+				}
+			});
+		});
+	}
+});
 
 //color-related functions
 function changeMapColor(yearRange, callback){
