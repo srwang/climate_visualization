0bb87782bdfe5b1fda06522dbf0f9922e0fe4bf0
Sharon Wag <sharon.ran.wang@gmail.com>
2016-07-09 12:59:21 -0400

    rotating, debugging double svg

--- public/js/main.js
+++ public/js/main.js
@@ -3,24 +3,32 @@ console.log('linked');
 
 //SET UP MAP
 var width = $(window).width(),
-    height = 800;
+    height = 600;
+
+var mousePosition0;
 
 //create SVG
 var svg = d3.select('#map').append('svg')
     .attr('width', width)
-    .attr('height', height);
+    .attr('height', height)
+    // .on('mousedown', function(){
+	   //  	mousePosition0 = [d3.event.pageX, d3.event.pageY];
+	   //  });
 
 //set map properties
-var projection = d3.geo.stereographic()
-    .scale(200)
+var projection = d3.geoOrthographic()
+    .scale(300)
+    .center([0, 0])
     .translate([width / 2, height / 2])
-    .rotate([-20, 0])
-    .clipAngle(130)
+    .rotate([0,0,0])
+    .clipAngle(100)
     .clipExtent([[0, 0], [width, height]])
     .precision(.1);
 
-var path = d3.geo.path()
+var path = d3.geoPath()
     .projection(projection);
+
+var feature;
 
 //RENDER MAP
 d3.json('map_data/new_world.json', function (error, world) {
@@ -33,7 +41,7 @@ d3.json('map_data/new_world.json', function (error, world) {
     .datum(subunits)
     .attr('d', path);
 
-	svg.selectAll('.subunit')
+	feature = svg.selectAll('.subunit')
 	    .data(topojson.feature(world, world.objects.subunits).features)
 	  .enter().append('path')
 	    .attr('class', 
@@ -69,57 +77,132 @@ d3.json('map_data/new_world.json', function (error, world) {
 			return 'translate(' + [center[0] - 20, center[1]] + ')';
 		})
 		.attr('dy', '.1em')
-		.style({'fill':'black', 'display':'none'})
+		.style('fill', 'black')
+		.style('display', 'none')
 		.text(function(d) { return d.properties.name; });
 
 	//display labels on hover
-	svg.selectAll('.subunit')
-		.on('mouseenter', function(){ 
-			svg.select('.subunit-label#' + this.id)
-				.style('display', 'block');
-		})
-		.on('mouseleave', function(){
-			svg.select('.subunit-label#' + this.id)
-				.style('display', 'none');
-		});
-});
+	// svg.selectAll('.subunit')
+	// 	.on('mouseenter', function(){ 
+	// 		svg.select('.subunit-label#' + this.id)
+	// 			.transition()
+	// 			.delay(200)
+	// 			.style('display', 'block');
+	// 	})
+	// 	.on('mouseleave', function(){
+	// 		svg.select('.subunit-label#' + this.id)
+	// 			.transition()
+	// 			.delay(200)
+	// 			.style('display', 'none');
+	// 	});
 
-//make sure upon refresh range-slider changes back to default!!!
+});	
+
+
+
+//fix rotation problem - 3 hours
+//put in modals with climate info - 1.5 hr
+
+//read projection info online
+//study 
+
+
+//click and drag
+	
+//begin animation on mousedown
+//change position on mousemove
+//stop animation on mouseup
+
+//don't return totally new position, but rather, different angle(??)
+svg.on('mouseup', function(){
+
+
+		// if (mousePosition0) {
+		// 	var currentCenter = projection.rotate(),
+		// 		mousePosition1 = [d3.event.pageX, d3.event.pageY],
+		// 		newCenter = [currentCenter[0] + (mousePosition0[0]-mousePosition1[0]) / 8, currentCenter[1] + (mousePosition1[1]-mousePosition0[1]) / 8];
+		// }
+
+		console.log(feature.attr('d'));
+
+	    projection.rotate([-d3.event.pageX, -d3.event.pageY, 0]);
+
+		feature.attr('d', d3.geoPath().projection(projection));
+
+		console.log(feature.attr('d'));
+
+		//calcColors();
+	});	
+
+
+
+// svg.selectAll('.subunit')
+// 	.transition()
+// 	.on('start', 'mouseup')
+// 	.tween('rotate', function(){
+// 		var center = [d3.event.pageX, d3.event.pageY],
+// 			r = d3.interpolate(projection.rotate(), [-center[0], -center[1]]);
+// 		return function(t) {
+// 			projection.rotate(r(t));
+// 		}
+// 	});
+
+//   (function transition() {
+//     d3.transition()
+//         .duration(1250)
+//         .each("start", function() {
+//           title.text(countries[i = (i + 1) % n].name);
+//         })
+//         .tween("rotate", function() {
+//           var p = d3.geo.centroid(countries[i]),
+//               r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+//           return function(t) {
+//             projection.rotate(r(t));
+//             c.clearRect(0, 0, width, height);
+//             c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
+//             c.fillStyle = "#f00", c.beginPath(), path(countries[i]), c.fill();
+//             c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+//             c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+//           };
+//         })
+//       .transition()
+//         .each("end", transition);
+//   })();
+// }
 
 
 //COLORS
 //set map to colors corresponding to 2020 temps
-changeMapColor([2020, 2039],
-	function(yearTwentyTemp, yearTwentyColor, countryCode){
-		setSvgFill(countryCode, yearTwentyColor);	
-});
+// changeMapColor([2020, 2039],
+// 	function(yearTwentyTemp, yearTwentyColor, countryCode){
+// 		setSvgFill(countryCode, yearTwentyColor);	
+// });
 
-//as temp changes over time, will increment or decrement reds to make change more visible
-$('#year-selector').change(function(){
-	var range = findYearRange();
-	$('#selector-label').text(range[0] + ' -- ' + range[1]);
+// //as temp changes over time, will increment or decrement reds to make change more visible
+// $('#year-selector').change(function(){
+// 	var range = findYearRange();
+// 	$('#selector-label').text(range[0] + ' -- ' + range[1]);
 
-	if (range[0] === 2020) {
-		changeMapColor(range, function(yearTwentyTemp, yearTwentyColor, countryCode){
-				setSvgFill(countryCode, yearTwentyColor);	
-		});
-	} else {
-		changeMapColor(range, function(currentTemp, countryCode) {
-			//make a second call per country, to find 2020 temp and colors
-			makeApiCall(countryCode, [2020, 2039], function(yearTwentyTemp, yearTwentyColor, countryCode){
+// 	if (range[0] === 2020) {
+// 		changeMapColor(range, function(yearTwentyTemp, yearTwentyColor, countryCode){
+// 				setSvgFill(countryCode, yearTwentyColor);	
+// 		});
+// 	} else {
+// 		changeMapColor(range, function(currentTemp, countryCode) {
+// 			//make a second call per country, to find 2020 temp and colors
+// 			makeApiCall(countryCode, [2020, 2039], function(yearTwentyTemp, yearTwentyColor, countryCode){
 
-				var tempDiff = currentTemp - yearTwentyTemp,
-					diffMult = Math.floor(tempDiff/0.5),
-					currentColor = [yearTwentyColor[0]+(20*diffMult), yearTwentyColor[1], yearTwentyColor[2]-(10*diffMult)];
+// 				var tempDiff = currentTemp - yearTwentyTemp,
+// 					diffMult = Math.floor(tempDiff/0.5),
+// 					currentColor = [yearTwentyColor[0]+(15*diffMult), yearTwentyColor[1], yearTwentyColor[2]-(10*diffMult)];
 
-				if(!isNaN(yearTwentyTemp)) {
-					console.log(tempDiff);
-					setSvgFill(countryCode, currentColor);
-				}
-			});
-		});
-	}
-});
+// 				if(!isNaN(yearTwentyTemp)) {
+// 					setSvgFill(countryCode, currentColor);
+// 				}
+// 			});
+// 		});
+// 	}
+// });
 
 //color-related functions
 function changeMapColor(yearRange, callback){
@@ -134,7 +217,7 @@ function changeMapColor(yearRange, callback){
 				makeApiCall(countryCode, yearRange, callback);
 			})(countryCode, yearRange);
 
-		} //fade effect
+		} 
 	});	
 }
 
@@ -148,7 +231,7 @@ function makeApiCall(countryCode, yearRange, callback) {
 			//create base color for 2020
 			var tempDiff = temp - 42,
 			diffMult = Math.floor(tempDiff / 2),
-			standardColor = [81, 126, 187],
+			standardColor = [101, 145, 177],
 			newColor = [standardColor[0] - (2 *(diffMult)), standardColor[1] - (4*(diffMult)), standardColor[2] - (7*(diffMult))];
 
 			callback(temp, newColor, countryCode);
@@ -162,8 +245,9 @@ function makeApiCall(countryCode, yearRange, callback) {
 function setSvgFill (countryCode, color) {
 	setTimeout(function(){
 			svg.selectAll('.subunit.' + countryCode)
+				.transition()
 				.style('fill', function(){ return 'rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')'});
-		}, 1000);
+		}, 1300);
 }
 
 function findYearRange() {
@@ -174,12 +258,19 @@ function findYearRange() {
 	return range;
 }
 
-//have possible values
-//on click, you can reset the colors of the map (and have a fade effect)
-	//click button is highlighted and others will dim
-	//2020-2039 is selected first by default
+//make globe
 
-//'about' side-bar - keys, explanation, gcm
+//menu bar
+	//explanation, gcm
+	//keys 
+
+//show modal on click
+	//show country name, 2020 average temp, average annual projected change, and next average temperature
+
+//add background circle image
+//add background text
+
+//make page responsive (?)
 
 
 
